65633ad53151524627f1c8084cc4126348fc74e3
Michael Babker <mbabker@flbab.com>
2010-09-13 01:11:31 +0000

    Remove "Restricted Access" message
    git-svn-id: http://joomlacode.org/svn/tweetdisplay/trunk@4 fc7e6c70-4d34-4316-a22e-8f1584383916

--- tmpl/nostyle.php
+++ tmpl/nostyle.php
@@ -11,7 +11,7 @@
 */
 
 // no direct access
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
 $headerAlign	= $params->get("headerAvatarAlignment");
 $tweetDisplay	= $params->get("tweetDisplayLocation");
