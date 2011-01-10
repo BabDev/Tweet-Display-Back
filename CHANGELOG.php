37af640b8ae557788cf07528a886635f2ef4324b
Michael Babker <mbabker@flbab.com>
2011-01-10 19:49:51 +0000

    Added attributes to <server> tag in trunk <updateservers>
    git-svn-id: http://joomlacode.org/svn/tweetdisplay/trunk@105 fc7e6c70-4d34-4316-a22e-8f1584383916

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -29,6 +29,9 @@ $ - Language fix or change
 - - Removed
 ! - Note
 
+10-January-2011 Michael Babker
+^ Added name and priority attributes to server tag for updateserver
+
 9-January-2011 Michael Babker
 # [#24242] Static function calls to non-static functions break module on PHP 5.3 (Thanks to Berend for the report)
 # Strict standards: Only variables should be assigned by reference mod_tweetdisplayback.php
