52f36eb01f4db149b36dc0573a22466576710cee
Michael Babker <mbabker@flbab.com>
2013-04-06 21:55:54 -0500

    Remove the Widget templates, Twitter no longer supports these

--- mod_tweetdisplayback.php
+++ mod_tweetdisplayback.php
@@ -25,79 +25,72 @@ $template       = $params->get('templateLayout', 'default');
 $flist          = ModTweetDisplayBackHelper::toAscii($params->get('twitterList', ''));
 $count          = $params->get('twitterCount', '3') - 1;
 
-// Don't load module CSS if loading a widget
-if ($params->get('twitterFeedType') != 'widget')
+// Load module CSS
+JHtml::stylesheet('mod_tweetdisplayback/' . $template . '.css', false, true, false);
+
+// Instantiate the helper
+$helper = new ModTweetDisplayBackHelper($params);
+
+// Check if caching is enabled
+if ($params->get('cache') == 1)
 {
-	JHtml::stylesheet('mod_tweetdisplayback/' . $template . '.css', false, true, false);
-}
+	// Fetch cache time from module parameters and convert to seconds
+	$cacheTime = $params->get('cache_time', 15);
+	$cacheTime = $cacheTime * 60;
 
-// If using a widget, don't need to perform custom module rendering
-if ($params->get('twitterFeedType') != 'widget')
-{
-	// Instantiate the helper
-	$helper = new ModTweetDisplayBackHelper($params);
+	// The files that the data is cached to
+	$cacheTweets = JPATH_CACHE . '/tweetdisplayback_tweets.json';
+	$cacheUser   = JPATH_CACHE . '/tweetdisplayback_user.json';
 
-	// Check if caching is enabled
-	if ($params->get('cache') == 1)
-	{
-		// Fetch cache time from module parameters and convert to seconds
-		$cacheTime = $params->get('cache_time', 15);
-		$cacheTime = $cacheTime * 60;
-
-		// The files that the data is cached to
-		$cacheTweets = JPATH_CACHE . '/tweetdisplayback_tweets.json';
-		$cacheUser   = JPATH_CACHE . '/tweetdisplayback_user.json';
-
-		// Cache files expired?
-		if ((!file_exists($cacheTweets) && !file_exists($cacheUser)) || (time() - @filemtime($cacheTweets) > $cacheTime && time() - @filemtime($cacheUser) > $cacheTime))
-		{
-			// Do a request to the Twitter API for new data
-			$twitter = $helper->compileData();
-		}
-		else
-		{
-			// Render from the cached data
-			$helper->isCached = true;
-			$twitter = $helper->compileFromCache();
-		}
-	}
-	else
+	// Cache files expired?
+	if ((!file_exists($cacheTweets) && !file_exists($cacheUser)) || (time() - @filemtime($cacheTweets) > $cacheTime && time() - @filemtime($cacheUser) > $cacheTime))
 	{
 		// Do a request to the Twitter API for new data
 		$twitter = $helper->compileData();
 	}
-
-	// Check to see if processing finished
-	if (!$helper->isProcessed)
+	else
 	{
-		// If we have cache files still, try to render from them
-		if (file_exists($cacheTweets) && file_exists($cacheUser))
-		{
-			// Render from the cached data
-			$helper->isCached = true;
-			$twitter = $helper->compileFromCache();
-		}
+		// Render from the cached data
+		$helper->isCached = true;
+		$twitter = $helper->compileFromCache();
+	}
+}
+else
+{
+	// Do a request to the Twitter API for new data
+	$twitter = $helper->compileData();
+}
 
-		// Check for error objects if processing did not finish
-		if (!$helper->isProcessed && isset($twitter['hits']))
-		{
-			echo '<div class="well well-small TDB-tweet' . $tweetClassSfx . '">'
-				. '<div class="TDB-tweet-container TDB-tweet-align-' . $tweetAlign . ' TDB-error">'
-				. '<div class="TDB-tweet-text">' . JText::_('MOD_TWEETDISPLAYBACK_ERROR_NOHITS') . '</div>'
-				. '</div></div>';
+// Check to see if processing finished
+if (!$helper->isProcessed)
+{
+	// If we have cache files still, try to render from them
+	if (file_exists($cacheTweets) && file_exists($cacheUser))
+	{
+		// Render from the cached data
+		$helper->isCached = true;
+		$twitter = $helper->compileFromCache();
+	}
 
-			return;
-		}
-		// No data object and no other error was set
-		elseif (!$helper->isProcessed && (!$twitter) || (isset($twitter['error'])))
-		{
-			echo '<div class="well well-small TDB-tweet' . $tweetClassSfx . '">'
-				. '<div class="TDB-tweet-container TDB-tweet-align-' . $tweetAlign . ' TDB-error">'
-				. '<div class="TDB-tweet-text">' . JText::_('MOD_TWEETDISPLAYBACK_ERROR_UNABLETOLOAD') . '</div>'
-				. '</div></div>';
+	// Check for error objects if processing did not finish
+	if (!$helper->isProcessed && isset($twitter['hits']))
+	{
+		echo '<div class="well well-small TDB-tweet' . $tweetClassSfx . '">'
+			. '<div class="TDB-tweet-container TDB-tweet-align-' . $tweetAlign . ' TDB-error">'
+			. '<div class="TDB-tweet-text">' . JText::_('MOD_TWEETDISPLAYBACK_ERROR_NOHITS') . '</div>'
+			. '</div></div>';
 
-			return;
-		}
+		return;
+	}
+	// No data object and no other error was set
+	elseif (!$helper->isProcessed && (!$twitter) || (isset($twitter['error'])))
+	{
+		echo '<div class="well well-small TDB-tweet' . $tweetClassSfx . '">'
+			. '<div class="TDB-tweet-container TDB-tweet-align-' . $tweetAlign . ' TDB-error">'
+			. '<div class="TDB-tweet-text">' . JText::_('MOD_TWEETDISPLAYBACK_ERROR_UNABLETOLOAD') . '</div>'
+			. '</div></div>';
+
+		return;
 	}
 }
 
@@ -112,24 +105,5 @@ if (!in_array('<script type="text/javascript" src="' . $scheme . 'platform.twitt
 	$document->addCustomTag('<script type="text/javascript" src="' . $scheme . 'platform.twitter.com/widgets.js"></script>');
 }
 
-// Add the Widgets script if needed
-if ($params->get('twitterFeedType') == 'widget')
-{
-	if (!in_array('<script type="text/javascript" src="' . $scheme . 'widgets.twimg.com/j/2/widget.js"></script>', $document->_custom))
-	{
-		$document->addCustomTag('<script type="text/javascript" src="' . $scheme . 'widgets.twimg.com/j/2/widget.js"></script>');
-	}
-}
-
-// Set the template to the correct option
-if ($params->get('twitterFeedType') == 'widget')
-{
-	$output = 'w_' . $params->get('templateWidget');
-}
-else
-{
-	$output = $template;
-}
-
 // Build the output
-require JModuleHelper::getLayoutPath('mod_tweetdisplayback', $output);
+require JModuleHelper::getLayoutPath('mod_tweetdisplayback', $template);
