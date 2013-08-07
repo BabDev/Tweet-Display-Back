dd3102c222105a446c62dc6bbba3d74f316f4c20
Michael Babker <mbabker@flbab.com>
2013-08-06 19:55:52 -0500

    Parse deleted files at each update, compress into a single method

--- script.php
+++ script.php
@@ -58,83 +58,23 @@ class Mod_TweetDisplayBackInstallerScript
 	 */
 	public function update($parent)
 	{
-		// Get the pre-update version
-		$version = $this->_getVersion();
-
-		// If in error, throw a message about the language files
-		if ($version == 'Error')
-		{
-			JError::raiseNotice(null, JText::_('MOD_TWEETDISPLAYBACK_ERROR_INSTALL_UPDATE'));
-
-			return;
-		}
-
-		// If coming from 2.1 or earlier, remove language files in system language folder and the media folder
-		if (version_compare($version, '3.0', 'lt'))
-		{
-			$this->_removeLanguageFiles();
-			$this->_removeMediaFolder();
-		}
-
-		// If coming from 3.0.0 or earlier, remove the compat files and widget templates (wasn't handled for 3.0.0)
-		if (version_compare($version, '3.0.1', 'lt'))
-		{
-			$this->_removeCompatFiles();
-			$this->_removeWidgets();
-		}
-
+		// Remove files deleted between versions
+		$this->_removeFiles();
 	}
 
 	/**
-	 * Function to get the currently installed version from the manifest cache
-	 *
-	 * @return  string  The version that is installed
-	 *
-	 * @since   3.0
-	 */
-	private function _getVersion()
-	{
-		// Get the record from the database
-		$db = JFactory::getDBO();
-		$query = $db->getQuery(true);
-		$query->select($db->quoteName('manifest_cache'));
-		$query->from($db->quoteName('#__extensions'));
-		$query->where($db->quoteName('element') . ' = ' . $db->quote('mod_tweetdisplayback'));
-		$db->setQuery($query);
-
-		if (!$db->loadObject())
-		{
-			JError::raiseWarning(1, JText::sprintf('JLIB_INSTALLER_ERROR_SQL_ERROR', $db->stderr(true)));
-			$version = 'Error';
-
-			return $version;
-		}
-		else
-		{
-			$manifest = $db->loadObject();
-		}
-
-		// Decode the JSON
-		$record = json_decode($manifest->manifest_cache);
-
-		// Get the version
-		$version = $record->version;
-
-		return $version;
-	}
-
-	/**
-	 * Function to remove the compatibility files introduced in 3.0.0 in favor of the BDHttp library
+	 * Function to remove files deleted between updates
 	 *
 	 * @return  void
 	 *
-	 * @since   3.0.1
+	 * @since   3.0
 	 */
-	private function _removeCompatFiles()
+	private function _removeFiles()
 	{
 		jimport('joomla.filesystem.file');
+		jimport('joomla.filesystem.folder');
 
-		// The files to remove
+		// Remove the connector compatibility files
 		$base  = JPATH_SITE . '/modules/mod_tweetdisplayback/';
 		$files = array('compat.php', 'curl.php', 'curl_25.php');
 
@@ -146,29 +86,15 @@ class Mod_TweetDisplayBackInstallerScript
 				JFile::delete($base . $file);
 			}
 		}
-	}
 
-	/**
-	 * Function to remove language files from the system language folder due to changing to
-	 * module language files for 3.0
-	 *
-	 * @return  void
-	 *
-	 * @since   3.0
-	 */
-	private function _removeLanguageFiles()
-	{
-		jimport('joomla.filesystem.file');
+		// Get the language tag for the site to delete non-English language files
+		$langTag = JFactory::getLanguage()->getTag();
 
-		$lang = JFactory::getLanguage();
-
-		$langCode = $lang->getTag();
-
-		$base    = JPATH_SITE . '/language/' . $langCode . '/';
+		$base    = JPATH_SITE . '/language/' . $langTag . '/';
 		$engBase = JPATH_SITE . '/language/en-GB/';
 
 		// The language files for pre-3.0
-		$files    = array($langCode . '.mod_tweetdisplayback.ini', $langCode . '.mod_tweetdisplayback.sys.ini');
+		$files    = array($langTag . '.mod_tweetdisplayback.ini', $langTag . '.mod_tweetdisplayback.sys.ini');
 		$engFiles = array('en-GB.mod_tweetdisplayback.ini', 'en-GB.mod_tweetdisplayback.sys.ini');
 
 		// Remove the files
@@ -188,38 +114,14 @@ class Mod_TweetDisplayBackInstallerScript
 				JFile::delete($engBase . $engFile);
 			}
 		}
-	}
 
-	/**
-	 * Function to remove the module's media folder due to restructuring in 3.0
-	 *
-	 * @return  void
-	 *
-	 * @since   3.0
-	 */
-	private function _removeMediaFolder()
-	{
-		jimport('joomla.filesystem.folder');
-
-		// Remove the folder if exists
+		// Remove the module's media folder if it exists
 		if (is_dir(JPATH_SITE . '/modules/mod_tweetdisplayback/media/'))
 		{
 			JFolder::delete(JPATH_SITE . '/modules/mod_tweetdisplayback/media/');
 		}
-	}
 
-	/**
-	 * Function to remove the widget templates
-	 *
-	 * @return  void
-	 *
-	 * @since   3.0.1
-	 */
-	private function _removeWidgets()
-	{
-		jimport('joomla.filesystem.file');
-
-		// The files to remove
+		// The widget template files to remove
 		$base  = JPATH_SITE . '/modules/mod_tweetdisplayback/tmpl/';
 		$files = array('w_list.php', 'w_profile.php', 'w_search.php');
 
